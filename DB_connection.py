5b3f0cbe9d420e8177289d59add1fd6b43b6096b
FrancescaCipolla <64315315+FrancescaCipolla@users.noreply.github.com>
2022-08-02 19:53:47 +0200

    modify db_connection ,json_parser, import_dataset
    Modificata funzione che controlla il nr di record nel db+chiamata alla funzione nel file di esecuzione Json_parser, tolto il break in import_dataset

--- DB_connection.py
+++ DB_connection.py
@@ -38,74 +38,20 @@ def connect_db (df,dfname):
 
 #interroga il db e stampa il numero di righe affette dalla query
 def count_row_from_db(df,table_name):
-    connection = psycopg2.connect(conn_str)
-    #connection.autocommit = True----->A COSA SERVE?
-    print (f"""Tabella {table_name}:""")
-    for column in df.columns:
-        sql_query=f"""SELECT {column} FROM {table_name} WHERE {column} IS NOT NULL"""
-        cursor = connection.cursor()
-        cursor.execute(sql_query)
-        print (f"""Nr righe non nulle per {column} = {cursor.rowcount}""")
-    cursor.close()
-    connection.close()
-
-
- 
-#USARE QUESTA!!!!!!interroga il db e stampa il numero di righe affette dalla query
-def count_row_from_db3(df,table_name):
     connection = create_conn(conn_str)
     #connection.autocommit = True----->A COSA SERVE?
     print (f"""Tabella {table_name}:""")
-    for column in df.columns:
-        sql_query=f"""SELECT {column} FROM {table_name} WHERE {column} IS NOT NULL"""
+    try:
+        for column in df.columns:
+            sql_query=f"""SELECT COUNT({column}) FROM {table_name}"""
+            results=connection.execute(sql_query)
+            print (f"""Nr righe non nulle per {column} = {results.fetchall()[0][0]}
+                    """) 
+    except AttributeError:
+        sql_query=f"""SELECT COUNT(*) FROM {table_name}"""
         results=connection.execute(sql_query)
-        print (f"""Nr righe non nulle per {column} = {results.rowcount}""") 
-    connection.close()   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-def count_row_from_db2(df,table_name):
-    connection = psycopg2.connect(conn_str)
-    #connection.autocommit = True----->A COSA SERVE?
-    print (f"""Tabella {table_name}:""")
-    for column in df.columns:
-        sql_query=f"""SELECT COUNT({column}) FROM {table_name}"""
-        cursor = connection.cursor()
-        cursor.execute(sql_query)
-        print (f"""Nr righe non nulle per {column} = {cursor.fetchone()}""")
-    cursor.close()
+        print (f"""Nr righe non nulle = {results.fetchall()[0][0]}
+                """) 
     connection.close()
 
-#interroga il db e scrive il risultato in un DataFrame
-def query_db(sql):
-    connection=create_conn(conn_str)
-    table=pd.read_sql_query(sql,connection)
-    connection.close()
-    return table
 
-
-#query
-sql_queries=['select * from paper',
-             'select * from alias_id',
-             'select * from with_alias',
-             'select * from reference',
-             'select * from venue',
-             'select * from author',
-             'select * from who',
-             'select * from author_extended',
-             'select * from fos',
-             'select * from what']
-
-#esegue una serie di query sul db
-def check_nb_records(sql_queries):
-    for query in sql_queries:
-        df=query_db(query)
-        df.info()
-        print()
